32c24c7804f1a528ac412f8d821bda88b728a25a
SLewis735 <selewis@fullsail.edu>
2015-03-11 19:11:31 -0400

    prompt and alert

---
+++ Screencast/Prompt and Alert/js/script.js
@@ -0,0 +1,5 @@
+///Prompt and Alert
+
+
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
--- Template/js/script.js
+++ Template/js/script.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-/**
- * Created by Superb1Mac on 3/11/15.
- */
-alert("hi, mom");
-=======
-//Expressions
-alert("hi");
 
+alert("hi, Sean");
 
->>>>>>> origin/master
