2a3f91355c6ebbd2ef9d51ded1a56dbd41ce2648
SLewis735 <selewis@fullsail.edu>
2015-03-11 19:30:18 -0400

    prompt and alert 2

--- Screencast/Prompt and Alert/js/script.js
+++ Screencast/Prompt and Alert/js/script.js
@@ -1,5 +1,10 @@
 ///Prompt and Alert
 
 
-var userInput = prompt("Enter your year of birth:");
-console.log(userInput);
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height  = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
+
