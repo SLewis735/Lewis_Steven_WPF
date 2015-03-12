b4e247fe1dff2108d05eec9f8705bc776f539436
SLewis735 <selewis@fullsail.edu>
2015-03-12 19:33:35 -0400

    commit 1

---
+++ Lewis_Steven_Expressions_Assignment/js/script.js
@@ -0,0 +1,5 @@
+// Steven Lewis 031215 Expressions Assignment
+
+var widthOfTheRoom = prompt("What is the width of the room?");
+var heightOfTheRoom = prompt("What is the height of the room?");
+var lengthOfTheRoom = prompt("what is the length the room?")
