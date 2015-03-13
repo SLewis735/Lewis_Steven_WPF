d8f824b2a6cbb9156c46e7d637ef33cf092699a1
SLewis735 <selewis@fullsail.edu>
2015-03-12 20:47:54 -0400

    commit 4

--- Lewis_Steven_Expressions_Assignment/js/script.js
+++ Lewis_Steven_Expressions_Assignment/js/script.js
@@ -5,6 +5,11 @@ var heightOfTheRoom = prompt("What is the height of the room?");
 var lengthOfTheRoom = prompt("what is the length the room?");
 var areaOfTheWalls = widthOfTheRoom * heightOfTheRoom * lengthOfTheRoom;
 var areaOfTheFloor = widthOfTheRoom * lengthOfTheRoom;
-var result = "The area of the walls to be painted " + areaOfTheWalls + " sq. ft. \nThe area of the floor to be carpeted " + areaOfTheFloor + " sq. ft.";
+var coveragePerGallon = 350;
+var gallonsOfPaint = areaOfTheWalls / coveragePerGallon;
+var percentageOfWaste = areaOfTheFloor / 10;
+var totalAmountOfCarpet = areaOfTheFloor + percentageOfWaste;
+var result = "You will need " + gallonsOfPaint + "gallons of paint and" + totalAmountOfCarpet + " square feet of carpet to complete this job";
+    //"The area of the walls to be painted " + areaOfTheWalls + " sq. ft. \nThe area of the floor to be carpeted " + areaOfTheFloor + " sq. ft.";
 
 console.log(result);
