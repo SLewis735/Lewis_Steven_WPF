1629bc5507ecc1644806e8490f1d080b946df88d
SLewis735 <selewis@fullsail.edu>
2015-03-12 23:04:20 -0400

    commit 9

--- Lewis_Steven_Expressions_Assignment/js/script.js
+++ Lewis_Steven_Expressions_Assignment/js/script.js
@@ -3,15 +3,20 @@
 var widthOfTheRoom = prompt("What is the width of the room?"); // This is the total width of the room.
 var heightOfTheRoom = prompt("What is the height of the room?"); //This is the height of the room.
 var lengthOfTheRoom = prompt("what is the length the room?"); // This is the length of the room.
-var areaOfTheWalls = widthOfTheRoom * heightOfTheRoom * lengthOfTheRoom; //The area of the walls to be painted.
-var areaOfTheFloor = widthOfTheRoom * lengthOfTheRoom; //The area of the floor to be carpeted.
-var coveragePerGallon = 350; //The average amount of coverage in a gallon of paint.
-var gallonsOfPaint = areaOfTheWalls / coveragePerGallon; //The number of gallons of paint that is needed.
-var percentageOfWaste = areaOfTheFloor / 10; // This is the allowance for waste.
-var totalAmountOfCarpet = areaOfTheFloor + percentageOfWaste; // This is the total square footage of carpet needed.
+var numberOfRooms = prompt("How many rooms per house"); //How many houses to be completed.
+numberOfRooms *= 3; //The number of houses to be completed.
+areaOfTheWalls = widthOfTheRoom * heightOfTheRoom * lengthOfTheRoom; //The area of the walls to be painted.
+areaOfTheFloor = widthOfTheRoom * lengthOfTheRoom; //The area of the floor to be carpeted.
+coveragePerGallon = 350; //The average amount of coverage in a gallon of paint.
+gallonsOfPaint = areaOfTheWalls / coveragePerGallon; //The number of gallons of paint that is needed.
+percentageOfWaste = areaOfTheFloor / 10; // This is the allowance for waste.
+totalAmountOfCarpet = areaOfTheFloor + percentageOfWaste; // This is the total square footage of carpet needed.
+
 var result = "You will need " + gallonsOfPaint + " gallons of paint and " + totalAmountOfCarpet + " square feet of carpet to complete this job.";
 // This is the results from the calculations of how many gallons of paint and how many square footage of carpet is needed to complete this job.
-
+var totalWork = "There are" + numberOfRoom + "number rooms that need to completed";
 
 console.log(result);
+console.log(totalWork);
 alert(result);
+alert(totalWork);
