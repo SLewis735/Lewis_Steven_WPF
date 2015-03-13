d67abfe8fc77ee245a8d539d80f4061a5d328154
SLewis735 <selewis@fullsail.edu>
2015-03-12 20:09:03 -0400

    commit 2

--- Lewis_Steven_Expressions_Assignment/js/script.js
+++ Lewis_Steven_Expressions_Assignment/js/script.js
@@ -2,4 +2,9 @@
 
 var widthOfTheRoom = prompt("What is the width of the room?");
 var heightOfTheRoom = prompt("What is the height of the room?");
-var lengthOfTheRoom = prompt("what is the length the room?")
+var lengthOfTheRoom = prompt("what is the length the room?");
+var areaOfTheWalls = widthOfTheRoom * heightOfTheRoom * lengthOfTheRoom;
+var areaOfTheFloor = widthOfTheRoom * lengthOfTheRoom;
+var result = "The area of the walls to be painted " + areaOfTheWalls + " sq. ft. \nThe area of the floor to be carpeted " + areaOfTheFloor + " sq. ft.";
+
+alert(result);
