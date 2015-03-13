05895923af19d6ad1bb55240589b25c4fe4edb49
SLewis735 <selewis@fullsail.edu>
2015-03-12 20:49:11 -0400

    commit 5

--- Lewis_Steven_Expressions_Assignment/js/script.js
+++ Lewis_Steven_Expressions_Assignment/js/script.js
@@ -9,7 +9,7 @@ var coveragePerGallon = 350;
 var gallonsOfPaint = areaOfTheWalls / coveragePerGallon;
 var percentageOfWaste = areaOfTheFloor / 10;
 var totalAmountOfCarpet = areaOfTheFloor + percentageOfWaste;
-var result = "You will need " + gallonsOfPaint + "gallons of paint and" + totalAmountOfCarpet + " square feet of carpet to complete this job";
+var result = "You will need " + gallonsOfPaint + " gallons of paint and " + totalAmountOfCarpet + " square feet of carpet to complete this job";
     //"The area of the walls to be painted " + areaOfTheWalls + " sq. ft. \nThe area of the floor to be carpeted " + areaOfTheFloor + " sq. ft.";
 
 console.log(result);
