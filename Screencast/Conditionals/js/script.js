9308f68e911831c4245beec4cfb07935b31053f7
SLewis735 <selewis@fullsail.edu>
2015-03-15 19:00:55 -0400

    screencast 2

--- Screencast/Conditionals/js/script.js
+++ Screencast/Conditionals/js/script.js
@@ -1,6 +1,7 @@
 //Conditional Logic - Relational Expressions
 
 var kidHeight =50;
+var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride!"
 // if the kid is over 48 inches in height
