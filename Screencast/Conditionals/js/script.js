18553d2d02323f0e9c8a30544af44e4eb44df5c0
SLewis735 <selewis@fullsail.edu>
2015-03-15 20:47:24 -0400

    ternary operators 2

--- Screencast/Conditionals/js/script.js
+++ Screencast/Conditionals/js/script.js
@@ -1,10 +1,15 @@
 //Conditional Logic - Ternary Operators
 
-var gpa = 48;
-//if the gpa is over the min 2.0 score, the student can graduate
-/*if( gpa > 2.0){
-    console.log("You can graduate!");
-}else{console.log("GPA is too low!");
-}*/
+var age = 11;
+var book;
 
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+if(age <10){
+    book = "Green Eggs and Ham";
+
+}else{
+    book = "The Time Machine";
+}
+
+console.log(book);
+
