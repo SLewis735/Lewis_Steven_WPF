ee6eeba8abc7427f4be891f69ee08176f052bc85
SLewis735 <selewis@fullsail.edu>
2015-03-15 20:27:19 -0400

    logical operator 6

--- Screencast/Conditionals/js/script.js
+++ Screencast/Conditionals/js/script.js
@@ -6,7 +6,7 @@ var wonLottery = true;
 
 //if the price of the phone is less the out budget AND if our paycheck is over 300
 
-if(iPhonePrice < budget || wonLottery === true){
+if(iPhonePrice < budget || wonLottery){
     console.log("We can buy the phone!");
 }else {
     console.log("No phone for you!!");
