114c5af3440dfc701d653893c00ee6bea7b00ce5
SLewis735 <selewis@fullsail.edu>
2015-03-18 19:31:13 -0400

    commit 2

--- Lewis_Steven_Conditionals_Assignment/js/script.js
+++ Lewis_Steven_Conditionals_Assignment/js/script.js
@@ -1,2 +1,6 @@
 // Steven Lewis 031715 Conditionals Assignment
-alert("hi");
+
+var costOfRent = prompt("How much are you paying for rent");
+var carNote = prompt("How much is your car note");
+var rateOfPay = prompt("What is you rate of pay per hour");
+var hoursOfWork = prompt("How many hours did you work");
