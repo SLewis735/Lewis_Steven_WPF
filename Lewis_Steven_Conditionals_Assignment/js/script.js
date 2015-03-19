a8610497c34e2f84d08fdf0a1e6e99a33cf4e770
SLewis735 <selewis@fullsail.edu>
2015-03-18 20:00:55 -0400

    commit 3

--- Lewis_Steven_Conditionals_Assignment/js/script.js
+++ Lewis_Steven_Conditionals_Assignment/js/script.js
@@ -2,5 +2,11 @@
 
 var costOfRent = prompt("How much are you paying for rent");
 var carNote = prompt("How much is your car note");
-var rateOfPay = prompt("What is you rate of pay per hour");
-var hoursOfWork = prompt("How many hours did you work");
+var rateOfPay = prompt("What is you rate of pay per hour \nYou are paid no more than $15 per hour");
+var hoursOfWork = prompt("How many hours did you work \nYou can work no more than 40 hours per week");
+var otherBills = 500;
+alert("Your additional bills are " + 500);
+
+budget = (costOfRent + carNote + otherBills);
+    console.log("Your budget for the month is" + budget);
+
