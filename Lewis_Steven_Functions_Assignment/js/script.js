56cb08d801fa577d730f8621b6525cd52aef9339
SLewis735 <selewis@fullsail.edu>
2015-03-26 03:44:10 -0400

    commit 1

--- Lewis_Steven_Functions_Assignment/js/script.js
+++ Lewis_Steven_Functions_Assignment/js/script.js
@@ -1,3 +1,31 @@
 //Steven Lewis 032515 Functions Assignment
 
-alert("hi");
+//variables
+var flLotto = prompt("Do you want to play the Florida Lottery?");
+
+// functions
+function gameValidation (lotto){
+    var timesClicked = 1;
+
+    while (lotto === "") {
+        lotto = prompt("Please enter 'yes' to play the Florida Lottery");
+
+        timesClicked++;
+
+        if (timesClicked === 3) {
+            console.log("Come back later to play the Florida Lottery.");
+            break;
+        }
+    }
+}
+
+
+
+//main code
+gameValidation(flLotto);
+if (flLotto = "yes"){
+    console.log("thank you");
+}
+
+
+
